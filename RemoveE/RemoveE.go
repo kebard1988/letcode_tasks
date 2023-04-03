831671b503e83c5467f3607b9dbb9bb5d8606011
kebard1988 <kebard1988@gmail.com>
2023-04-03 21:05:04 +0300

    fix dublicat

--- RemoveE/RemoveE.go
+++ RemoveE/RemoveE.go
@@ -1,9 +1,6 @@
 package removee
 
 func removeElement(nums []int, val int) int {
-	if nums == nil || len(nums) == 0 {
-		return 0
-	}
 	res := 0
 	for i := 0; i < len(nums); i++ {
 		if nums[i] != val {

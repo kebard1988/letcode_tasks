63993faa9af87dc0ecc55af1453156ab22508667
kebard1988 <kebard1988@gmail.com>
2023-04-04 21:40:42 +0300

    add for index

--- ReverseString/ReverseString.go
+++ ReverseString/ReverseString.go
@@ -1,9 +1,7 @@
 package reversestring
 
-import "fmt"
-
 func reverseString(s []byte) {
-	for i := len(s)-1; i>=0; i-- {
-		fmt.Println(s[i])
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
 	}
 }
